f79a8bdbecad851fdc96ecd5287bcb4890443d27
Kristin Baumann <hello@kristin-baumann.com>
2025-02-05 14:34:41 +0100

    Test change

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-console.log("Hello, World!");
+console.log("Hello, World!!!");
 
 const svg = d3
   .select("#chart-container")
   .append("svg")
   .attr("viewBox", "0 0 200 200")
   .style("border", "1px solid black")
-  .style("background-color", "lightgrey");
+  .style("background-color", "pink");
